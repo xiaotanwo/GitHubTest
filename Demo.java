a29e3ea1486c5ad21da550d5e3606aba03b0f227
xiaotan <645743118@qq.com>
2020-04-05 17:40:39 +0800

    Demo.java update master

--- Demo.java
+++ Demo.java
@@ -2,24 +2,24 @@ class Solution {
     public int trap(int[] height) {
         int left = 0, right = height.length -1;
         int left_max = 0, right_max = 0;
-        int res = 0;
+        int ans = 0;
         while (left < right) {
             if (height[left] < height[right]) {
                 if (height[left] >= left_max) {
                     left_max = height[left];
                 } else {
-                    res += left_max - height[left];
+                    ans += left_max - height[left];
                 }
                 left++;
             } else {
                 if (height[right] >= right_max) {
                     right_max = height[right];
                 } else {
-                    res += right_max - height[right];
+                    ans += right_max - height[right];
                 }
                 right--;
             }
         }
-        return res;
+        return ans;
     }
 }
